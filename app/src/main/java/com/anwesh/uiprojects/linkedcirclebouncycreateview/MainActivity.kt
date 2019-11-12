09762f3dd6d528de65c0cba986a1983f540f5c50
Anwesh43 <anweshthecool0@gmail.com>
2019-11-12 19:27:52 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcirclebouncycreateview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcirclebouncycreateview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcirclebouncycreateview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.circlebouncycreateview.CircleBouncyCreateView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleBouncyCreateView.create(this)
     }
 }
