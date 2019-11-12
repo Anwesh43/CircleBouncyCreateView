c8b33f41e82dd95ea587b2c42609e4aa56bdae6b
Anwesh43 <anweshthecool0@gmail.com>
2019-11-12 19:21:34 +0530

    adding CircleBouncyCreate class

--- circlebouncycreateview/src/main/java/com/anwesh/uiprojects/circlebouncycreateview/CircleBouncyCreateView.kt
+++ circlebouncycreateview/src/main/java/com/anwesh/uiprojects/circlebouncycreateview/CircleBouncyCreateView.kt
@@ -161,4 +161,28 @@ class CircleBouncyCreateView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class CircleBouncyCreate(var i : Int) {
+
+        private val root : CBCNode = CBCNode(0)
+        private var curr : CBCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
